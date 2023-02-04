2ac0fea7a3a2a8a494374cab51c7d2d58d433424
ejc10d <112822833+ejc10d@users.noreply.github.com>
2023-02-04 10:25:34 -0500

    added user data to comments

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,14 +1,16 @@
 const commentForm = async (event) => {
-    event.prefentDefault();
+    event.preventDefault();
 
     const post_id = document.querySelector('.new-comment-form').dataset.postid;
 
     const comment_content = document.querySelector('#comment_content').value.trim();
 
+    const user_id = document.querySelector('.new-comment-form').dataset.userid;
+
     if (comment_content) {
         await fetch(`/api/comments`, {
             method: 'POST',
-            body: JSON.stringify({ post_id, comment_content }),
+            body: JSON.stringify({ post_id, comment_content, user_id }),
             headers: { 'Content-Type': 'application/json' },
         });
             document.location.reload();
